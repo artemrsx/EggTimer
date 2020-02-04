bac5b28d9e786f4687e65785135267526356a04d
Bochkarev Artem <[bochkarev.av@net868.ru]>
2020-02-04 23:34:14 +0300

    switch/if/else/dictionaries

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let softTime = 5;
+    let mediumTime = 7;
+    let hardTime = 12;
+    
+    let eggTimes = [
+        "Soft": 5,
+        "Medium": 7,
+        "Hard": 12
+    ]
+    
+    @IBAction func onEggPress(_ sender: UIButton) {
+        var time: Int;
+        let title: String = sender.currentTitle!;
 
+        print(eggTimes[title]!);
+        
+        switch title {
+        case "Soft":
+            time = 5;
+        case "Medium":
+            time = 7;
+        case "Hard":
+            time = 12;
+        default:
+            time = 0;
+        }
+        
+        print(title, time);
+    }
+    
 
 }
