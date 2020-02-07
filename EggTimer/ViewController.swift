5d1df85f18a2ee4a137226658390ba18321b52d1
Bochkarev Artem <[bochkarev.av@net868.ru]>
2020-02-07 12:58:02 +0300

    добавил проценты к прогресс бару

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -3,22 +3,28 @@ import UIKit
 class ViewController: UIViewController {
     
     let eggTimes = [
-        "Soft": 3,
-        "Medium": 4,
-        "Hard": 7
+        "Soft": 300,
+        "Medium": 420,
+        "Hard": 720
     ]
     var totalTime = 0;
     var secondsPassed = 0;
     var timer = Timer();
     
     @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var progressLabel: UILabel!
     @IBOutlet weak var appTitle: UILabel!
     @objc func update() {
         if (secondsPassed < totalTime) {
+            let progress = Float(secondsPassed)/Float(totalTime);
+            let progressText = String(Int(progress * 100)) + " %";
             secondsPassed += 1;
-            progressBar.progress = Float(secondsPassed)/Float(totalTime);
+            progressBar.progress = progress;
+            progressLabel.text = progressText;
         } else {
-            appTitle.text = "DONE !!!"
+            appTitle.text = "DONE !!!";
+            progressLabel.text = "100 %";
+            progressBar.progress = 1;
             timer.invalidate();
         }
     }
@@ -32,6 +38,7 @@ class ViewController: UIViewController {
         appTitle.text = title;
         secondsPassed = 0;
         progressBar.progress = 0;
+        progressLabel.text = "0 %";
         
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(update), userInfo: nil, repeats: true)
     }
